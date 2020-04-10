556752f083ef6679009fd3913c392e414a980275
Nikita Govokhin <33430830+NikitaDestrain@users.noreply.github.com>
2020-04-10 14:58:08 +0400

    Update script.sql

--- src/main/resources/script.sql
+++ src/main/resources/script.sql
@@ -1,4 +1,4 @@
-create table IF NOT EXISTS users
+create table IF NOT EXISTS users # todo lower case o_O
 (
     user_id            serial PRIMARY KEY,
     login              VARCHAR(20) UNIQUE,
@@ -6,7 +6,7 @@ create table IF NOT EXISTS users
     role               VARCHAR(20),
     dateOfRegistration timestamptz
 );
-create table IF NOT EXISTS journals
+create table IF NOT EXISTS journals # todo lower case o_O
 (
     journal_id    serial PRIMARY KEY,
     user_id       INTEGER references users (user_id),
@@ -16,7 +16,7 @@ create table IF NOT EXISTS journals
     isPrivate     boolean,
     UNIQUE (name, user_id)
 );
-CREATE TABLE IF NOT EXISTS tasks
+CREATE TABLE IF NOT EXISTS tasks # todo upper case O_o
 (
     task_id      serial PRIMARY KEY,
     journal_id   INTEGER REFERENCES journals (journal_id) ON DELETE CASCADE,
