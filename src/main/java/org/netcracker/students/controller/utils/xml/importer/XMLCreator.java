53b9d6cba2aac8a247e85bc63839976ee122320e
JustTrag <il.kozlov98@gmail.com>
2020-05-01 04:19:47 +0400

    Simple example xml for imports and XMLCreator.class with void methods.

---
+++ src/main/java/org/netcracker/students/controller/utils/xml/importer/XMLCreator.java
@@ -0,0 +1,14 @@
+package org.netcracker.students.controller.utils.xml.importer;
+
+import org.netcracker.students.model.Journal;
+import org.netcracker.students.model.Task;
+
+import java.util.List;
+
+public class XMLCreator {
+    public void createJournalXml(String path, Journal journal){}
+
+    public void createTaskXml(String path, Task task){}
+
+    public void createJournalWithTaskXml(String path, Journal journal, List<Task> tasks) {}
+}
