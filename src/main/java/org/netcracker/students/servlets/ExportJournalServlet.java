b62812d60f1c4edf2e1fda37534f8a8db2841ca6
Desire456 <selickiy.vladislav@gmail.com>
2020-05-18 01:27:42 +0400

    Fix export files and servlet ExportJournalServlet

--- src/main/java/org/netcracker/students/servlets/ExportJournalServlet.java
+++ src/main/java/org/netcracker/students/servlets/ExportJournalServlet.java
@@ -6,18 +6,13 @@ import org.netcracker.students.servlets.constants.MappingConstants;
 import org.netcracker.students.servlets.constants.ServletConstants;
 
 import javax.ejb.EJB;
-import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.ServletOutputStream;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.File;
-import java.io.FileWriter;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -34,33 +29,16 @@ public class ExportJournalServlet extends HttpServlet {
         } catch (ExportException e) {
             e.printStackTrace();
         }
-
-        String xml = "<Hello></Hello>";
-
-        File file = new File("asd.xml");
-
-            FileWriter fileWriter = new FileWriter(file, false);
-            fileWriter.write(xml);
-            ServletContext context = getServletContext();
-            ServletOutputStream out = resp.getOutputStream();
-            byte[] bytes = xml.getBytes();
-            resp.setHeader("Content-Disposition", "attachment; filename=\"" + "example.xml"+ "\"");
-            out.write(bytes);
-            out.flush();
-            out.close();
-
-
-       /* try {
-            PropertyParser propertyParser = PropertyParser.getInstance();
-            File file = new File(propertyParser.getProperty("uploadPath") + "journal.xml");
-            if (!file.exists()) file.mkdirs();
-
-        } catch (PropertyFileException e) {
-            e.printStackTrace();
-        }*/
-
-
-            resp.sendRedirect(MappingConstants.JOURNALS_PAGE_MAPPING);
+        String xml = "asd";
+        String fileName = req.getParameter(ServletConstants.PARAMETER_FILENAME);
+        ServletOutputStream out = resp.getOutputStream();
+        byte[] bytes = xml.getBytes();
+        resp.setHeader(ServletConstants.RESP_HEADER_NAME, ServletConstants.RESP_HEADER_VALUE + fileName
+                + ServletConstants.FILE_EXTENSION);
+        out.write(bytes);
+        out.flush();
+        out.close();
+        resp.sendRedirect(MappingConstants.JOURNALS_PAGE_MAPPING);
     }
 
     private List<Integer> parseStrIds(String ids) {
