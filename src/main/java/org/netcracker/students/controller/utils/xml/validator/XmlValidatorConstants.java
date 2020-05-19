de3557bf9a8f6f1fedcf6d12dad6c632828825a2
JustTrag <il.kozlov98@gmail.com>
2020-05-19 05:19:50 +0400

    Change constants language from Russian to English

--- src/main/java/org/netcracker/students/controller/utils/xml/validator/XmlValidatorConstants.java
+++ src/main/java/org/netcracker/students/controller/utils/xml/validator/XmlValidatorConstants.java
@@ -1,8 +1,9 @@
 package org.netcracker.students.controller.utils.xml.validator;
 
 public class XmlValidatorConstants {
-    public static final String XML_NOT_FOUND_MESSAGE = "Не найден xml файл";
-    public static final String XSD_NOT_FOUND_MESSAGE = "Не найден xsd файл";
-    public static final String XML_NOT_VERIFIED_MESSAGE = "Xml не соответствует указанной схеме";
+    public static final String XML_NOT_FOUND_MESSAGE = "Can't find xml file";
+    public static final String XSD_NOT_FOUND_MESSAGE = "Can't find xsd-schema";
+    public static final String XML_NOT_VERIFIED_MESSAGE = "XML doesn't match specified xsd-schema";
     public static final String PATH_TO_XSD = "xml/xmlExample.xsd";
 }
+
